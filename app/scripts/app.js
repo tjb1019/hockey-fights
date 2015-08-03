576e1d790eefcf2c848c8590cacf441bc04e4640
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2015-08-03 01:42:48 -0600

    Added initial files

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hockeyFightsApp
+ * @description
+ * # hockeyFightsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hockeyFightsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hockeyFightsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hockeyFightsApp
+ */
+angular.module('hockeyFightsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
