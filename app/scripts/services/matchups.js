63632505b7b4137186d6a8d3c8467422a1b94027
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2015-08-11 00:01:23 -0700

    added more player matches

--- app/scripts/services/matchups.js
+++ app/scripts/services/matchups.js
@@ -18,5 +18,45 @@ angular.module('hockeyFightsApp')
           name: 'Matthew Barnaby',
           img: '../../images/matthew-barnaby.jpg'
         }
+    },
+    {
+        player1: {
+          name: 'Tie Domi',
+          img: '../../images/tie-domi.jpg'
+        },
+        player2: {
+          name: 'Donald Brashear',
+          img: '../../images/donald-brashear.jpg'
+        }
+    },
+    {
+        player1: {
+          name: 'Georges Laraque',
+          img: '../../images/georges-laraque.png'
+        },
+        player2: {
+          name: 'Colton Orr',
+          img: '../../images/colton-orr.jpg'
+        }
+    },
+    {
+        player1: {
+          name: 'Eric Lindros',
+          img: '../../images/eric-lindros.jpg'
+        },
+        player2: {
+          name: 'Scott Stevens',
+          img: '../../images/scott-stevens.jpg'
+        }
+    },
+    {
+        player1: {
+          name: 'Andre Roy',
+          img: '../../images/andre-roy.jpg'
+        },
+        player2: {
+          name: 'Riley Cote',
+          img: '../../images/riley-cote.jpg'
+        }
     }
   ]);
