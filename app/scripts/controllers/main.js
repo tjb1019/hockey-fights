9cc40a21767cfca5ae27be47da410214f0e65155
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2015-08-11 13:42:22 -0700

    fixed image relative paths

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,7 +21,7 @@ angular.module('hockeyFightsApp')
     $scope.secondGuyImg = thisRound.player2.img;
 
     // Make API call now while user is making a selection
-    var hipCheck = sinBin.query({keywords: $scope.firstGuyName + '+' + $scope.secondGuyName});
+    var hipCheck = sinBin.query({keywords: $scope.firstGuyName + '+' + $scope.secondGuyName + 'fight'});
     hipCheck.$promise.then(function(data) {
         var videoId = data.items[0].id.videoId;
         var thisVideo = '<iframe width="420" height="315" src="http://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen class="center-block"></iframe>';
--- app/scripts/services/matchups.js
+++ app/scripts/services/matchups.js
@@ -12,51 +12,51 @@ angular.module('hockeyFightsApp')
   	{
         player1: {
           name: 'Peter Worrell',
-          img: '../../images/peter-worrell.jpg'
+          img: '../images/peter-worrell.jpg'
         },
         player2: {
           name: 'Matthew Barnaby',
-          img: '../../images/matthew-barnaby.jpg'
+          img: '../images/matthew-barnaby.jpg'
         }
     },
     {
         player1: {
           name: 'Tie Domi',
-          img: '../../images/tie-domi.jpg'
+          img: '../images/tie-domi.jpg'
         },
         player2: {
           name: 'Donald Brashear',
-          img: '../../images/donald-brashear.jpg'
+          img: '../images/donald-brashear.jpg'
         }
     },
     {
         player1: {
           name: 'Georges Laraque',
-          img: '../../images/georges-laraque.png'
+          img: '../images/georges-laraque.png'
         },
         player2: {
           name: 'Colton Orr',
-          img: '../../images/colton-orr.jpg'
+          img: '../images/colton-orr.jpg'
         }
     },
     {
         player1: {
           name: 'Eric Lindros',
-          img: '../../images/eric-lindros.jpg'
+          img: '../images/eric-lindros.jpg'
         },
         player2: {
           name: 'Scott Stevens',
-          img: '../../images/scott-stevens.jpg'
+          img: '../images/scott-stevens.jpg'
         }
     },
     {
         player1: {
           name: 'Andre Roy',
-          img: '../../images/andre-roy.jpg'
+          img: '../images/andre-roy.jpg'
         },
         player2: {
           name: 'Riley Cote',
-          img: '../../images/riley-cote.jpg'
+          img: '../images/riley-cote.jpg'
         }
     }
   ]);
