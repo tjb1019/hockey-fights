a5e58439fd048cccbd5a0afb4d2770cee0c46b2b
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2015-08-31 01:20:45 -0700

    lowered possible video count to improve accuracy

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,8 +20,8 @@ angular.module('hockeyFightsApp')
     // Make API call now while user is making a selection
     var hipCheck = sinBin.query({keywords: $scope.firstGuyName + '+' + $scope.secondGuyName + 'fight'});
     hipCheck.$promise.then(function(data) {
-        // Choose 1 of 3 possible videos so user can see different fights with the same players
-        var videoId = data.items[Math.floor(Math.random() * 2)].id.videoId;
+        // Choose 1 of 2 possible videos so user can see different fights with the same players
+        var videoId = data.items[Math.floor(Math.random() * 1)].id.videoId;
         // Generate iframe code and insert into resource for gametime view to use.  Insert player names for current matchup as well
         var thisVideo = '<iframe width="420" height="315" src="http://www.youtube.com/embed/' + videoId + '?autoplay=1" frameborder="0" allowfullscreen class="center-block" id="youtube-player"></iframe>';
         videoData.videoHTML = thisVideo;
